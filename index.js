9b5546c5d2f0001b4615c18d1c30f32daca0b1ed
Haolong <chl123.chenhaolong@gmail.com>
2025-01-22 12:01:32 -0500

    finish

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const dns = require('dns');
+require("url")
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,30 +20,48 @@ app.get('/', function(req, res) {
 });
 
 const urls = [];
+let count = 0;
 
 app.post('/api/shorturl', (req, res) => {
-  let url = req.body.url.replace(/\/*$/, '');
-  let validUrl = url.replace(/^https:\/\/(www.)?/, '');
-  console.log(validUrl);
+  let url;
+  try{
+    url = new URL(req.body.url);
+  }
+  catch(e) {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+  if(url.protocol !== 'http:' && url.protocol !== 'https:') {
+    res.json({ error: 'invalid url' });
+    return;
+  }
+  const validUrl = url.hostname;
   dns.lookup(validUrl, (err, address, family) => {
-    if (err || url.substring(0, 4) !== 'http') {
+    if (err) {
       res.json({ error: 'invalid url' })
     }
     else {
-      if (!urls.includes(url)) {
-        urls.push(url);
+      if (!urls.includes(req.body.url)) {
+        urls.push(req.body.url);
+        count++;
+        console.log(urls, count);
       }
       res.json({
-        original_url: url,
-        short_url: urls.indexOf(url) + 1
+        original_url: req.body.url,
+        short_url: urls.indexOf(req.body.url) + 1
       });
     }
   });
 });
 
 app.get('/api/shorturl/:id', (req, res) => {
-  const externarlUrl = urls[req.params.id - 1];
-  res.redirect(externarlUrl);
+  if(req.params.id > count) {
+    res.json({ error: 'No short URL found for the given input' });
+  }
+  else{
+    const externarlUrl = urls[req.params.id - 1];
+    res.redirect(externarlUrl);
+  }
 });
 
 app.listen(port, function() {
